ec270a15e9285d03d74d27ff77dbecd174ae3cb0
Dan Orban <dtorban@umn.edu>
2019-01-09 15:21:07 -0600

    Added standard library.

--- project/project.cmake
+++ project/project.cmake
@@ -31,3 +31,28 @@ macro (addStandardExecutable)
 		     ARCHIVE DESTINATION lib
 		     RUNTIME DESTINATION bin)
 endmacro()
+
+macro (addStandardLibrary)
+	if(DEP_INCLUDES)
+		include_directories(${DEP_INCLUDES})
+	endif()
+
+	if(DEP_LINK_DIRS)
+		link_directories(${DEP_LINK_DIRS})
+	endif()
+
+	add_library (${PROJECT_NAME} ${HEADERFILES} ${SOURCEFILES} ${DEP_SOURCEFILES})
+
+	if(PROJECT_DEPS)
+		add_dependencies(${PROJECT_NAME} ${PROJECT_DEPS})
+	endif()
+
+	target_link_libraries(${PROJECT_NAME} ${DEP_LIBS} ${ALL_LIBS})
+
+	# --------------------- Install -----------------------
+
+	install( TARGETS ${PROJECT_NAME}
+		     LIBRARY DESTINATION lib
+		     ARCHIVE DESTINATION lib
+		     RUNTIME DESTINATION bin)
+endmacro()
