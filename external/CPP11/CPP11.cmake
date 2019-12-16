8dfe17214689addcb21acdf8352762a10ebff002
Dan Orban <dtorban@umn.edu>
2019-12-16 13:39:11 -0600

    Added c++17

--- external/CPP11/CPP11.cmake
+++ external/CPP11/CPP11.cmake
@@ -27,3 +27,31 @@ macro (useCPP11)
 		set(ALL_LIBS ${ALL_LIBS} ${CMAKE_THREAD_LIBS_INIT} rt Xrandr Xxf86vm m dl ${X11_LIBRARIES})
 	endif()
 endmacro()
+
+
+macro (useCPP17)
+	# Windows Section #
+	if (MSVC)
+		add_definitions(-D_CRT_SECURE_NO_WARNINGS)
+		# Tell MSVC to use main instead of WinMain for Windows subsystem executables
+		set_target_properties(${WINDOWS_BINARIES} PROPERTIES LINK_FLAGS "/ENTRY:mainCRTStartup")
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+	endif()
+
+	if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+		set(CMAKE_CXX_FLAGS "-std=c++11 -stdlib=libc++")
+		set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++17")
+		set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++")
+	endif()
+
+	if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+	  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.7")
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++0x")
+	  else(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.7")
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17")
+	  endif(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.7")
+		find_package(Threads)
+		find_package(X11)
+		set(ALL_LIBS ${ALL_LIBS} ${CMAKE_THREAD_LIBS_INIT} rt Xrandr Xxf86vm m dl ${X11_LIBRARIES})
+	endif()
+endmacro()
